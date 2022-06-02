ff1796208dbc25d46fb951dcee7c8ca9895fbbd2
Jozef Mlich <jmlich83@gmail.com>
2022-06-02 10:47:52 +0200

    lupdate

--- translations/glacier-alarm-listener.ts
+++ translations/glacier-alarm-listener.ts
@@ -1,37 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1"  language="en" >
+<TS version="2.1" language="en">
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../src/qml/pages/MainPage.qml" line="17"/>
-        <location filename="../src/qml/pages/MainPage.qml" line="17"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="19"/>
         <source>Alarm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/MainPage.qml" line="29"/>
-        <location filename="../src/qml/pages/MainPage.qml" line="29"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="38"/>
         <source>No alarm is active</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/MainPage.qml" line="48"/>
-        <location filename="../src/qml/pages/MainPage.qml" line="48"/>
-        <source>Dismiss</source>
+        <location filename="../src/qml/pages/MainPage.qml" line="75"/>
+        <source>Dismiss All</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/MainPage.qml" line="59"/>
-        <location filename="../src/qml/pages/MainPage.qml" line="59"/>
-        <source>Snooze</source>
+        <location filename="../src/qml/pages/MainPage.qml" line="86"/>
+        <source>Snooze All</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/main.cpp" line="67"/>
+        <location filename="../src/main.cpp" line="30"/>
         <source>Alarm</source>
         <translation type="unfinished"></translation>
     </message>
